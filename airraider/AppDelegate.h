672bc39d97fe3d6a753646ed72c077e7901cba8b
robinsun <s3441078@student.rmit.edu.au>
2014-08-22 17:48:43 +1000

    Initial Commit

---
+++ airraider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  airraider
+//
+//  Created by wilson on 8/22/14.
+//  Copyright (c) 2014 wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
